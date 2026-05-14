e79a9e5713d786e1a99f31e6b5bd26a5d029e906
biancapazudesa <pazb@udesa.edu.ar>
2026-05-14 17:34:21 -0300

    Transforme el componente Home para que funcione con hooks

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,61 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState } from "react"
+import { useEffect } from "react"
 import Card from "../../components/Card/Card"
 import Buscador from '../../components/Buscador/Buscador'
 import Header from "../../components/Header/Header"
 
-class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            populares: [],
-            cartel: [],
-            seriesPopulares: [],
-            seriesCartel: []
-        }
-    }
+function Home (props){
+    const [populares, setPopulares] = useState([])
+    const [cartel, setCartel] = useState([])
+    const [seriesPopulares, setSeriesPopulares] = useState([])
+    const [seriesCartel, setSeriesCartel] = useState([]) 
 
-    componentDidMount() {
+    useEffect( ()=> {
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=34bbb0b5f876dc4dae13f205c0163fd0")
             .then(response => response.json())
-            .then(data => this.setState(
-                { populares: data.results }
-            ))
+            .then(data => setPopulares(data.results))
             .catch(error => console.log(error))
 
         fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=34bbb0b5f876dc4dae13f205c0163fd0")
             .then(response => response.json())
-            .then(data => this.setState(
-                { cartel: data.results }
-            ))
+            .then(data => setCartel(data.results))
             .catch(error => console.log(error))
 
         fetch("https://api.themoviedb.org/3/tv/popular?api_key=34bbb0b5f876dc4dae13f205c0163fd0")
             .then(response => response.json())
-            .then(data => this.setState(
-                { seriesPopulares: data.results }
-            ))
+            .then(data => setSeriesPopulares(data.results))
             .catch(error => console.log(error))
 
         fetch("https://api.themoviedb.org/3/tv/airing_today?api_key=34bbb0b5f876dc4dae13f205c0163fd0")
             .then(response => response.json())
-            .then(data => this.setState(
-                { seriesCartel: data.results }
-            ))
+            .then(data => setSeriesCartel(data.results))
             .catch(error => console.log(error))
 
-    }
+    },[])
 
-    render() {
         return (
             <>
                 <Header />
                 <Buscador />
                 <section className='sectionCard'>
                     <h1 className="categoria">Películas más populares</h1>
-                    {this.state.populares.length === 0 ?
+                    {populares.length === 0 ?
                         <h3>Cargando...</h3> :
                         <ul className="contenedorUl">
-                            {this.state.populares.filter((pelicula, i) => i < 4).map((pelicula, idx) => (
+                            {populares.filter((pelicula, i) => i < 4).map((pelicula, idx) => (
                                 <li key={pelicula.title + idx}><Card
                                     titulo={pelicula.title}
                                     img={pelicula.poster_path}
@@ -68,10 +56,10 @@ class Home extends Component {
                     }
 
                     <h1 className="categoria">Películas en cartel</h1>
-                    {this.state.cartel.length === 0 ?
+                    {cartel.length === 0 ?
                         <h3>Cargando...</h3> :
                         <ul className="contenedorUl">
-                            {this.state.cartel.filter((pelicula, i) => i < 4).map((pelicula) => (
+                            {cartel.filter((pelicula, i) => i < 4).map((pelicula) => (
                                 <li><Card
                                     key={pelicula.id}
                                     titulo={pelicula.title}
@@ -85,10 +73,10 @@ class Home extends Component {
                     }
 
                     <h1 className="categoria2">Series mas populares</h1>
-                    {this.state.seriesPopulares.length === 0 ?
+                    {seriesPopulares.length === 0 ?
                         <h3>Cargando...</h3> :
                         <ul className="contenedorUl">
-                            {this.state.seriesPopulares.filter((serie, i) => i < 4).map((serie) => (
+                            {seriesPopulares.filter((serie, i) => i < 4).map((serie) => (
                                 <li><Card
                                     key={serie.id}
                                     titulo={serie.name}
@@ -102,10 +90,10 @@ class Home extends Component {
                     }
 
                     <h1 className="categoria2">Series en cartel</h1>
-                    {this.state.seriesCartel.length === 0 ?
+                    {seriesCartel.length === 0 ?
                         <h3>Cargando...</h3> :
                         <ul className="contenedorUl">
-                            {this.state.seriesCartel.filter((serie, i) => i < 4).map((serie) => (
+                            {seriesCartel.filter((serie, i) => i < 4).map((serie) => (
                                 <li><Card
                                     key={serie.id}
                                     titulo={serie.name}
@@ -120,6 +108,6 @@ class Home extends Component {
                 </section></>
         )
     }
-}
+
 
 export default Home
