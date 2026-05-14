68203322c2bf854ce018149ea218e87a4bebae39
Belen Bruni <bbruni@udesa.edu.ar>
2026-05-14 17:07:28 -0300

    Modifique el componente Menu para que este programado con hooks

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState } from "react"
 import { Link } from 'react-router-dom'
 import './styles.css'
 import Cookies from 'universal-cookie'
 const cookies = new Cookies()
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-    }
+function Menu(props) {
+    console.log('cookies', cookies.get('user-auth-cookie'))
+    return (
+        <nav className='menu'>
+            <div className="izquierda">
+                <Link to='/'>Home</Link>
+                <Link to='/Peliculas'>Peliculas</Link>
+                <Link to='/Series'>Series</Link>
+                {cookies.get('user-auth-cookie') ? <Link to='/Favoritas'>Favoritas</Link> : null}
+            </div>
 
-    render() {
-        console.log('cookies', cookies.get('user-auth-cookie'))
-        return (
-            <nav className='menu'>
-                <div className="izquierda">
-                    <Link to='/'>Home</Link>
-                    <Link to='/Peliculas'>Peliculas</Link>
-                    <Link to='/Series'>Series</Link>
-                    {cookies.get('user-auth-cookie') ? <Link to='/Favoritas'>Favoritas</Link> : null}
-                </div>
-
-                {cookies.get('user-auth-cookie') ? null :
+            {cookies.get('user-auth-cookie') ? null :
                 <div className="derecha">
                     <Link to='/Login' className='linkDos'>Iniciar sesión</Link>
                     <Link to='/Register' className='linkDos'>Registrarse</Link>
                 </div>}
-            </nav>)
-    }
+        </nav>)
 }
 
 export default Menu
