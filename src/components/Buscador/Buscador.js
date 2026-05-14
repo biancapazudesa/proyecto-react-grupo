30e8e42efe2bc6034a0733a1cccada5545aeca14
biancapazudesa <pazb@udesa.edu.ar>
2026-05-14 16:42:49 -0300

    Tranforme al componente Buscador con hooks.

--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -1,57 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {useState} from "react"
 import { withRouter } from "react-router-dom";
 import './styles.css'
 
-class Buscador extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            valor: '',
-            tipo: ''
-        }
-    }
+function Buscador(props){
+    const [valor, setValor] = useState("")
+    const [tipo, setTipo] = useState("")
 
-    evitarSubmit(e) {
+    function evitarSubmit(e) {
         e.preventDefault();
-        this.props.history.push(
-            `/resultados/${this.state.tipo}/${this.state.valor}`
+        props.history.push(
+            `/resultados/${tipo}/${valor}`
         )
     }
 
-    controlarCambios(e) {
-        this.setState({
-            valor: e.target.value
-        })
+    function controlarCambios(e) {
+        setValor(e.target.value) 
     }
 
-    tipoDeBusquedaSerie(e) {
-        this.setState({
-            tipo: 'tv'
-        })
+    function tipoDeBusquedaSerie(e) {
+        setTipo("tv")
     }
 
-    tipoDeBusquedaPelicula(e) {
-        this.setState({
-            tipo: 'movie'
-        })
+    function tipoDeBusquedaPelicula(e) {
+        setTipo("movie")
     }
 
-    render() {
-        return (
-            <form className="search-form" onSubmit={(e) => this.evitarSubmit(e)}>
-                <input onChange={(e) => this.controlarCambios(e)} type="text" className="barra" name="searchData" placeholder="Buscar..." value={this.state.valor} />
-                <button type="submit" className="btn btn-success btn-sm">Buscar</button>
-                <section className='opciones'>
-                    <input onChange={(e) => this.tipoDeBusquedaPelicula(e)} type='radio' value='movie' name='searchType' className='radios' />
-                    <label>Películas</label>
-                    <input onChange={(e) => this.tipoDeBusquedaSerie(e)} type='radio' value='tv' name='searchType' className='radios' />
-                    <label>Series</label>
-                </section>
-            </form>
-
-
-        )
-    }
+    return (
+        <form className="search-form" onSubmit={(e) => evitarSubmit(e)}>
+            <input onChange={(e) => controlarCambios(e)} type="text" className="barra" name="searchData" placeholder="Buscar..." value={valor} />
+            <button type="submit" className="btn btn-success btn-sm">Buscar</button>
+            <section className='opciones'>
+                <input onChange={(e) => tipoDeBusquedaPelicula(e)} type='radio' value='movie' name='searchType' className='radios' />
+                <label>Películas</label>
+                <input onChange={(e) => tipoDeBusquedaSerie(e)} type='radio' value='tv' name='searchType' className='radios' />
+                <label>Series</label>
+            </section>
+        </form>
+    )
 }
 
 export default withRouter(Buscador);
